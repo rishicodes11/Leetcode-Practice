7c1b2f4ef4de856c6bc462a9f02314f45328d530
rishicodes11 <125854913+rishicodes11@users.noreply.github.com>
2025-03-25 21:09:09 +0530

    Time: 9 ms (21.68%), Space: 24.7 MB (23.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>new_interval;
         sort(intervals.begin(),intervals.end());
-        for(auto it:intervals){
-            if(new_interval.size()==0) {
-                new_interval.push_back(it);
-                continue; //skipping the first one from below if 
-                }
-
-            if(it[0]<=new_interval.back()[1]){
-                new_interval.back()[1]=max(it[1],new_interval.back()[1]);
+        vector<vector<int>>new_interval;
+        for(auto it: intervals){
+            if(new_interval.empty() || it[0] > new_interval.back()[1]){
+                //agar khali hai ya interval is like (1,2) (4,6) then
+                //basically we cant merge so push only 
+            new_interval.push_back(it);
             }
-            else{
-                new_interval.push_back(it);
+            else{ //push krna hai, first wala min hi rhega as sort kr lia hai
+            new_interval.back()[1]=max(new_interval.back()[1],it[1]);
+
             }
         }
-    return new_interval;
+        return new_interval;
     }
 };
