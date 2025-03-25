b8bb45f7ebe0bd6824f108f19c423cc081f438a0
rishicodes11 <125854913+rishicodes11@users.noreply.github.com>
2025-03-25 21:53:52 +0530

    Time: 0 ms (100%), Space: 28.8 MB (87.57%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-     //merege intervals
-     // traverse again 
-     //count total no of points
-     //subtract it from the max one ie new_interval.back()[1]; 
-     //nah this subtract one is not needed 
-     //but like fuck it if i do it like this it will take on tc+ and on sc but i want o1 sc, lets see what i can do 
-    //brute
-    // (1,2) (4,6)
-    sort(nums.begin(),nums.end());
-    vector<vector<int>>new_interval;
-    for(auto &it: nums){
-        if(new_interval.size()==0 || it[0] > new_interval.back()[1] ){
-            new_interval.push_back(it);
+        //basically we optimised it for space stuff 
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        int start=nums[0][0],end=nums[0][1];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i][0]<=end){
+                end=max(end,nums[i][1]);
+            }
+            else{
+                sum+=end-start+1;
+                start=nums[i][0];
+                end=nums[i][1];
+            }
         }
-        else{
-            new_interval.back()[1]=max(new_interval.back()[1],it[1]);
-        }
-    }
-    // now we have the new interval
-    // traverse over it and get total points
-    int sum=0;
-    for(auto it: new_interval){
-        sum+= (it[1]-it[0]+1);
-    }
-    return sum;
+        sum+=(end-start+1);
+        return sum;
     }
 };
