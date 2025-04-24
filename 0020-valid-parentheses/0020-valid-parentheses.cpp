534db8d9737597dfb4682ed90cbef3191037262e
rishicodes11 <125854913+rishicodes11@users.noreply.github.com>
2025-04-24 14:23:45 +0530

    Time: 0 ms (100%), Space: 8.8 MB (62.39%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char it : s) {
+            if (it == '(' || it == '[' || it == '{') {
+                st.push(it);
+            } else {
+                if (st.empty()) return false;
+
+                if ((it == ')' && st.top() == '(') ||
+                    (it == ']' && st.top() == '[') ||
+                    (it == '}' && st.top() == '{')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty(); // if anything's left, it's invalid
+    }
+};
