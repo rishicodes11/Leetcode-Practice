ef3d85ff50a905e0738f79aa79fb0b3bf2eb48a9
Rohan Raj <125854913+rishicodes11@users.noreply.github.com>
2025-09-11 09:45:08 +0530

    Time: 27 ms (13.45%), Space: 18.8 MB (15.08%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        //brute
-        //get all the vowels from the string in another string and sort them first
-        // but to get vowels i need to match em so should i create a set or just do if else?
-        //since its low i can create a set as it will take n=10 only
-        multiset<char>st;
-        for(auto it: s){
-            if(it=='A'||it=='E'||it=='I'||it=='O'||it=='U'||
-            it=='a'||it=='e'||it=='i'||it=='o'||it=='u'){
-                st.insert(it);
+        vector<int> freq(256,0);
+        for(char c:s){
+            if(isVowel(c)){
+                freq[c]++;
             }
         }
         string t;
-        auto ite=st.begin();
-        for(auto it:s){
-            if(it=='A'||it=='E'||it=='I'||it=='O'||it=='U'||
-            it=='a'||it=='e'||it=='i'||it=='o'||it=='u'){
-                t.push_back(*ite);
-                ite++;
-            }
+        for(char c:s){
+            if(isVowel(c)){
+                for(int ch=0;ch<256;ch++){
+                    if(freq[ch]>0){
+                        t.push_back((char)ch);
+                        freq[ch]--;
+                        break;
+                    }
+                }
+            } 
             else{
-                t.push_back(it);
+                t.push_back(c);
             }
         }
         return t;
     }
+
+private:
+    bool isVowel(char c){
+        return c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||
+        c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+    };
 };
